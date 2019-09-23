ec91a683904f84d3be27eceaa31f9275c9df8d6e
Luca Violanti <luca.violanti@gmail.com>
2019-09-24 00:36:39 +0100

    Solve exercises 1 and 2 of Chapter 9

--- src/main/scala/Chapter09.scala
+++ src/main/scala/Chapter09.scala
@@ -3,20 +3,34 @@ object Chapter09 {
   def reverseLinesInAFile(pathToFile: String): List[String] = {
     import scala.io.Source
     import scala.collection.mutable._
-    val lines = Source.fromFile(pathToFile).getLines()
-    var reversedLines = Stack[String]()
-    for (l <- lines) reversedLines = Stack(l)++reversedLines
-    reversedLines.toList
+    val file = Source.fromFile(pathToFile)
+    val reversedLines = file.getLines().toList.reverse
+    file.close()
+    reversedLines
+    // Try it with `reverseLinesInAFile("src/main/resources/file-to-reverse.txt")`.
   }
+
   // 2. Write a Scala program that reads a file with tabs, replaces each tab with spaces so that tab stops are at
   // n-column boundaries, and writes the result to the same file.
+  def replaceTabsWithSpaces(pathToFile: String, nColumns: Int = 2): Unit = {
+    import scala.io.Source
+    import scala.collection.mutable._
+    val file = Source.fromFile(pathToFile)
+    val lines = file.getLines
+    for (l <- lines) {
+      println(l.replaceAll("\t", " " * nColumns))
+      // Don't want to write to the same file.
+    }
+    file.close()
+    // Try it with `replaceTabsWithSpaces("src/main/resources/file-with-tabs.txt", 1)`.
+  }
 
   // 3. Write a Scala code snippet that reads a file and prints all words with more than 12 characters to the console.
   // Extra credit if you can do this in a single line.
 
   // 4. Write a Scala program that reads a text file containing only floating-point numbers.
   // Print the sum, average, maximum, and minimum of the numbers in the file.
-  
+
   // 5. Write a Scala program that writes the powers of 2 and their reciprocals to a file, with the exponent ranging
   // from 0 to 20. Line up the columnsL
   //    1     1
@@ -35,7 +49,7 @@ object Chapter09 {
 
   // 9. Write a Scala program that counts how many files with `.class` extension are in a given directory and its
   // subdirectories.
-  
+
   // 10. Expand the example in Section 9.8, "Serialization", on page 113. Construct a few `Person` objects, make
   // some of them friends of others, and save an `Array[Person]` to a file. Read the array back in and verify that
   // the friend relations are intact.
