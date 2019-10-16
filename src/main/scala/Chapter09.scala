df7db886a7f17fe2121706ee6f323f066d4d0b9c
Luca Violanti <luca.violanti@gmail.com>
2019-10-16 22:59:04 +0100

    Solve Exercises 7 and 8 in Chapter 9

--- src/main/scala/Chapter09.scala
+++ src/main/scala/Chapter09.scala
@@ -1,9 +1,10 @@
+import java.io.File
+import scala.io.Source
 import scala.math.Ordering.Double
 
 object Chapter09 {
   // 1. Write a Scala code snippet that reverses the lines in a file (making the last line the first one and so on).
   def reverseLinesInAFile(pathToFile: String): List[String] = {
-    import scala.io.Source
     val file = Source.fromFile(pathToFile)
     val reversedLines = file.getLines().toList.reverse
     file.close()
@@ -14,7 +15,6 @@ object Chapter09 {
   // 2. Write a Scala program that reads a file with tabs, replaces each tab with spaces so that tab stops are at
   // n-column boundaries, and writes the result to the same file.
   def replaceTabsWithSpaces(pathToFile: String, nColumns: Int = 2): Unit = {
-    import scala.io.Source
     val file = Source.fromFile(pathToFile)
     val lines = file.getLines
     for (l <- lines) {
@@ -28,7 +28,6 @@ object Chapter09 {
   // 3. Write a Scala code snippet that reads a file and prints all words with more than 12 characters to the console.
   // Extra credit if you can do this in a single line.
   def printAllWordsWithMoreThan12Characters(pathToFile: String): Unit = {
-    import scala.io.Source
     val file = Source.fromFile(pathToFile)
     file.mkString.split("\\W+").filter(s => s.length > 12).foreach(println)
     file.close()
@@ -38,7 +37,6 @@ object Chapter09 {
   // 4. Write a Scala program that reads a text file containing only floating-point numbers.
   // Print the sum, average, maximum, and minimum of the numbers in the file.
   def printSumAverageMaximumAndMinimum(pathToFile: String): Unit = {
-    import scala.io.Source
     implicit val ordering = Ordering.Double.TotalOrdering
     val file = Source.fromFile(pathToFile)
     val lines = file.getLines().toList
@@ -67,7 +65,6 @@ object Chapter09 {
   // 6. Make a regular expression searching for quoted strings "like this, maybe with \" or \\" in a Java or C++
   // program. Write a Scala program that prints out all such strings in a source file.
   def searchQuotedStrings(pathToFile: String): Unit = {
-    import scala.io.Source
     val file = Source.fromFile(pathToFile)
     val quoted = """\"(\\.|[^\"])*\"""".r
     quoted.findAllIn(file.mkString).foreach(println)
@@ -77,12 +74,27 @@ object Chapter09 {
 
   // 7. Write a Scala program that reads a text file and prints all tokens in the file that are not floating point
   // numbers. Use a regular expression.
+  def searchAllButFPNumbers(pathToFile: String): Unit = {
+    val file = Source.fromFile(pathToFile)
+    val fpNumbers = "[0-9]+?\\.[0-9]+".r
+    fpNumbers.findAllIn(file.mkString).foreach(println)
+    file.close()
+    // Try it with `searchAllButFPNumbers("src/main/resources/file-with-tabs.txt")`
+  }
 
   // 8. Write a Scala program that prints the `src` attributes of all `img` tags of a web page.
   // Use regular expressions and groups.
+  def searchImageSources(pathToFile: String): Unit = {
+    val file = Source.fromFile(pathToFile)
+    val sources = "<img.*src=(?:\"|')([^\"']+).*>".r
+    sources.findAllIn(file.mkString).matchData.foreach(m => println(m.group(1)))
+    file.close()
+    // Try it with `searchImageSources("src/main/resources/sample.html")`
+  }
 
   // 9. Write a Scala program that counts how many files with `.class` extension are in a given directory and its
   // subdirectories.
+  def countClassFilesInDirectoryTree(pathToDirectory: String): Int = ???
 
   // 10. Expand the example in Section 9.8, "Serialization", on page 113. Construct a few `Person` objects, make
   // some of them friends of others, and save an `Array[Person]` to a file. Read the array back in and verify that
